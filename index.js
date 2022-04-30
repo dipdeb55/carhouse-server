b8745d05084ae66d09320be3555986a9463a0510
Xenon_xod <dipdeb55@gmail.com>
2022-04-30 18:11:31 +0600

    inventory api added

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// DB_PASSWORD=rkYcBBmeI1fAX03K
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://deep:PhnCF0OuymW8hZxv@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const carCollection = client.db("assignment-11").collection("cars")
+
+        app.get('/cars', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars)
+        })
+
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carCollection.findOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
