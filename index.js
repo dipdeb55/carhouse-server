a22b5cefd6d20808af73538f4e5ca2f18d080150
Xenon_xod <dipdeb55@gmail.com>
2022-05-07 19:01:47 +0600

    heroku adding

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // const uri = `mongodb+srv://deep:qTvgrz1igsMmhpCW@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
