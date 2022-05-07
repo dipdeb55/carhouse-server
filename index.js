9037cca96b0d38b015d145f6848de42adfe6a5b9
Xenon_xod <dipdeb55@gmail.com>
2022-05-07 14:00:47 +0600

    env added

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-// DB_PASSWORD=rkYcBBmeI1fAX03K
-
 app.use(cors());
 app.use(express.json());
 
 
-// const uri = `mongodb+srv://DB_USER:DB_PASS@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://deep:qTvgrz1igsMmhpCW@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://deep:qTvgrz1igsMmhpCW@cluster0.ncmem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
